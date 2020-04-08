d1181cabba6b020e65464efb0a1d4d4a0ffe565b
GADIFFET <theo.gadiffet@leroymerlin.fr>
2020-04-08 14:15:12 +0200

    add(page): Add all of necessary pages

---
+++ page-accueil-renseignement.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire</title>
+    <link rel="stylesheet" type="text/css" href="formulaire-professionnel.css">
+</head>
+<body>
+    <!--En-tête du formulaire-->
+    <div class="en-tete">
+        <div class="logo">
+            <img src="img/logo.PNG">
+        </div>
+        <div class="titre">
+            <div class="nom-entreprise">
+                ConnectLife
+            </div>
+            <div class="slogan">
+                Que la force vous guide
+            </div>
+        </div>
+    </div>
+    <!--Formulaire-->
+    <div class="formulaire">
+        <div class="checkbox">
+            Civilité
+            <input type="checkbox"> Madame
+            <input type="checkbox"> Monsieur
+        </div>
+        <div class="input">
+            Nom <input>
+        </div>
+        <div class="input">
+            Prenom <input>
+        </div>
+        <div class="input">
+            Nom de la société <input>
+        </div>
+        <div class="input">
+            Poste occupé <input>
+        </div>        
+        <div class="input">
+            Adresse1 <input>
+        </div>
+        <div class="input">
+            Adresse2 <input>
+        </div>
+        <div class="input">
+            Code Postale <input>
+        </div>
+        <div class="input selection">
+            Ville <input>
+        </div>
+        <div class="input">
+            Téléphone Société <input>
+        </div>
+        <div class="input">
+            Téléphone Directe <input>
+        </div>
+        <div class="input">
+            Mail <input>
+        </div>
+    </div>
+    <!--Validation-->
+    <div class="validation">
+        <button> Valider </button>
+    </div>
+</body>
+</html>
