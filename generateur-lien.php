7bb75b70d151e67244e24b95e1ab01b3945433a0
GADIFFET <theo.gadiffet@leroymerlin.fr>
2020-05-30 15:29:48 +0200

    feat(GUID):Add - at GUID generate

--- generateur-lien.php
+++ generateur-lien.php
@@ -1,7 +1,17 @@
 <?php
 session_start();
 
-$guid = bin2hex(random_bytes(16));
+function GUID()
+{
+    if (function_exists('com_create_guid') === true)
+    {
+        return trim(com_create_guid(), '{}');
+    }
+
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
+$guid = GUID();
 
 $_POST['guid'] = $guid;
 
